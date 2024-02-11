a38e40259480f0bf8261058dbe1eca3beece3ef4
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-02-10 22:45:51 -0600

    fix whitespace issue

--- overrides/kubejs/server_scripts/tags.js
+++ overrides/kubejs/server_scripts/tags.js
@@ -1,9 +1,9 @@
 onEvent('item.tags', event => {
-    event.add('forge:computation_matrix', 'kubejs:computation_matrix')
-    event.add('forge:enderium_machine', 'kubejs:enderium_machine')
-    event.add('forge:controller', 'ae2:controller')
-    event.add('forge:iron_block', 'minecraft:iron_block')
-	})
+	event.add('forge:computation_matrix', 'kubejs:computation_matrix')
+	event.add('forge:enderium_machine', 'kubejs:enderium_machine')
+	event.add('forge:controller', 'ae2:controller')
+	event.add('forge:iron_block', 'minecraft:iron_block')
+})
 
 onEvent('block.tags', event => {
 	event.add('minecraft:beacon_base_blocks', 'alloyed:steel_block')
