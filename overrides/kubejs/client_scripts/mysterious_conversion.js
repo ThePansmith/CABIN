8f6d9670eb06dfc85334e55375276095e2b6a510
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-07-14 12:44:39 -0500

    fix mysterious conversion script

--- overrides/kubejs/client_scripts/mysterious_conversion.js
+++ overrides/kubejs/client_scripts/mysterious_conversion.js
@@ -1,6 +1,14 @@
-let MysteriousItemConversionCategory = java('com.simibubi.create.compat.jei.category.MysteriousItemConversionCategory')
-let ConversionRecipe = java('com.simibubi.create.compat.jei.ConversionRecipe')
-//Chapter 3 singularity recipe
-MysteriousItemConversionCategory.RECIPES.add(ConversionRecipe.create('ae2:quantum_entangled_singularity', 'ae2:singularity')).id('kubejs:mysterious_conversion/quantum_entangled_singularity')
-//Refined radiance mysterious conversion
-MysteriousItemConversionCategory.RECIPES.add(ConversionRecipe.create('create:chromatic_compound', 'create:refined_radiance')).id('kubejs:mysterious_conversion/refined_radiance')
+// let MysteriousItemConversionCategory = java('com.simibubi.create.compat.jei.category.MysteriousItemConversionCategory')
+const ConversionRecipe = java('com.simibubi.create.compat.jei.ConversionRecipe')
+
+//We have to brute force things since the kubejs create guide doesn't work at all
+onEvent('jei.remove.recipes', event => {
+    const mysteryConversion = global.jeiRuntime.jeiHelpers.getRecipeType('create:mystery_conversion').get()
+		global.jeiRuntime.recipeManager.addRecipes(mysteryConversion, [
+			//Chapter 3 singularity recipe
+			new ConversionRecipe.create('ae2:singularity', 'ae2:quantum_entangled_singularity'),
+			//Refined radiance mysterious conversion
+			new ConversionRecipe.create('create:chromatic_compound', 'create:refined_radiance'),
+			new ConversionRecipe.create('create:chromatic_compound', 'create:shadow_steel')
+		]);
+})
