c80598ef00f340c8b1202501633bc36562a1915c
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-08-08 17:11:16 -0500

    Change andesite chunkloader compatability recipe to cost a ghast tear instead of a wither skeleton skull

--- overrides/kubejs/server_scripts/server_compatability/create_power_loader.js
+++ overrides/kubejs/server_scripts/server_compatability/create_power_loader.js
@@ -1,6 +1,6 @@
 if (Platform.isLoaded('create_power_loader')) { 
 	onEvent('recipes', event => {
+		andesiteMachine(event,Item.of('create_power_loader:empty_andesite_chunk_loader', 1), 'minecraft:ghast_tear')
 		brassMachine(event,Item.of('create_power_loader:empty_brass_chunk_loader', 1), 'minecraft:nether_star')
-		andesiteMachine(event,Item.of('create_power_loader:empty_andesite_chunk_loader', 1), 'minecraft:wither_skeleton_skull')
 	})
 }
