9a61e7086591c7117c74ce15229900c53d3da15f
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-07 23:22:47 -0400

    Add files via upload

---
+++ overrides/kubejs/server_scripts/server_compatability/balancedflight.js
@@ -0,0 +1,13 @@
+if(Platform.isLoaded("balancedflight")) {
+	onEvent('recipes', event => {
+	event.remove({ output: 'balancedflight:flight_anchor' })
+	event.recipes.createSequencedAssembly([
+	   'balancedflight:flight_anchor',
+	], 'minecraft:beacon', [
+		event.recipes.createDeploying(MC('beacon'), [MC('beacon'), KJ('inductive_mechanism')]),
+		event.recipes.createDeploying(MC('beacon'), [MC('beacon'), CR('railway_casing')]),
+		event.recipes.createDeploying(MC('beacon'), [MC('beacon'), CR('shaft')]),
+		event.recipes.createDeploying(MC('beacon'), [MC('beacon'), MC('elytra')]),
+	]).loops(1)
+  })
+}
