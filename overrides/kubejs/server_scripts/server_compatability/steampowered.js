3c64a067b6c10b151f79107c63e92556558ceaeb
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-08 20:01:24 -0400

    Steam Powered compat

---
+++ overrides/kubejs/server_scripts/server_compatability/steampowered.js
@@ -0,0 +1,49 @@
+if(Platform.isLoaded("steampowered")) {
+	onEvent('recipes', event => {
+
+    // Extended Gears already has metalic cogwheels, so Steam Powered cogs are redudant
+    // Craft & Additions already has two different alternators, so Steam Powered dynamo is redudant
+    event.remove({ mod: 'steampowered' })
+
+    // Burners
+    let burner = (output, metalBurner) => { 
+      event.shaped(output, [
+        'III',
+        'I I',
+        'BBB'
+      ], {
+        B: 'minecraft:bricks',
+        I: metalBurner
+      })
+    }
+    burner('steampowered:bronze_burner', 'alloyed:bronze_sheet')
+    burner('steampowered:cast_iron_burner', 'createdeco:cast_iron_sheet')
+    burner('steampowered:steel_burner', 'alloyed:steel_sheet')
+
+    // Boilers
+    let boiler = (output, metalBoiler) => {
+      event.shaped(output, [
+        ' M ',
+        'MTM',
+        ' M '
+      ], {
+        T: 'create:fluid_tank',
+        M: metalBoiler
+      })
+    }
+    boiler('steampowered:bronze_boiler', 'alloyed:bronze_sheet')
+    boiler('steampowered:cast_iron_boiler', 'createdeco:cast_iron_sheet')
+    boiler('steampowered:steel_boiler', 'alloyed:steel_sheet')
+
+    // Engines
+    brassMachine(event,Item.of('steampowered:bronze_steam_engine'), 'alloyed:bronze_block')
+		brassMachine(event,Item.of('steampowered:cast_iron_steam_engine'), 'createdeco:cast_iron_block')
+    brassMachine(event,Item.of('steampowered:steel_steam_engine'), 'alloyed:steel_block')
+
+    // Flywheels
+    event.smithing('steampowered:bronze_flywheel', 'create:flywheel', 'alloyed:bronze_block')
+    event.smithing('steampowered:cast_iron_flywheel', 'create:flywheel', 'createdeco:cast_iron_block')
+    event.smithing('steampowered:steel_flywheel', 'create:flywheel', 'alloyed:steel_block')
+  })
+}
+
