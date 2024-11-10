725db7c2a225a16201d4d1fcbdc4494ec42f2028
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-10 01:34:59 -0400

    Missing Bronze Sheet.js

--- overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
+++ overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
@@ -10,4 +10,5 @@ if (Platform.isLoaded("createaddition")) {
 
 	global.randomiumBlacklist.push("steampowered:pressurized_gas_container")
 	global.randomiumBlacklist.push("steampowered:pressurized_steam_container")
+	global.randomiumBlacklist.push("steampowered:bronze_sheet")
 }
