f2ab2b5f9736a37996fb116f6ff587a42c07059a
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-10 01:21:07 -0400

    Steam Powered Blacklist

---
+++ overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
@@ -0,0 +1,13 @@
+// Steam Powered
+if (Platform.isLoaded("createaddition")) {
+	global.itemBlacklist.push("steampowered:alternator")
+	global.itemBlacklist.push("steampowered:bronze_cogwheel")
+	global.itemBlacklist.push("steampowered:bronze_large_cogwheel")
+	global.itemBlacklist.push("steampowered:cast_iron_cogwheel")
+	global.itemBlacklist.push("steampowered:cast_iron_large_cogwheel")
+	global.itemBlacklist.push("steampowered:steel_cogwheel")
+	global.itemBlacklist.push("steampowered:steell_large_cogwheel")
+
+	global.randomiumBlacklist.push("steampowered:pressurized_gas_container")
+	global.randomiumBlacklist.push("steampowered:pressurized_steam_container")
+}
