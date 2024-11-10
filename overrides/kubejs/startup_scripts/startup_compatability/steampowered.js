71746e216e3636845ebe7026c2c6099bc5d22328
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-10 01:22:13 -0400

    Small typo.js

--- overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
+++ overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
@@ -6,7 +6,7 @@ if (Platform.isLoaded("createaddition")) {
 	global.itemBlacklist.push("steampowered:cast_iron_cogwheel")
 	global.itemBlacklist.push("steampowered:cast_iron_large_cogwheel")
 	global.itemBlacklist.push("steampowered:steel_cogwheel")
-	global.itemBlacklist.push("steampowered:steell_large_cogwheel")
+	global.itemBlacklist.push("steampowered:steel_large_cogwheel")
 
 	global.randomiumBlacklist.push("steampowered:pressurized_gas_container")
 	global.randomiumBlacklist.push("steampowered:pressurized_steam_container")
