8f616017851fcc8205e36046bee7377bb3ff6a79
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-11 18:00:25 -0400

    Steam Powered Compat Overhaul

--- overrides/kubejs/server_scripts/server_compatability/steampowered.js
+++ overrides/kubejs/server_scripts/server_compatability/steampowered.js
@@ -1,49 +1,70 @@
 if(Platform.isLoaded("steampowered")) {
 	onEvent('recipes', event => {
 
-    // Extended Gears already has metalic cogwheels, so Steam Powered cogs are redundant
-    // Craft & Additions already has two different alternators, so Steam Powered dynamo is redudant
-    event.remove({ mod: 'steampowered' })
+    event.remove({ mod: 'steampowered' }) // The recipes are mostly outdated, reminiscent of Create 0.3
 
-    // Burners
-    let burner = (output, metalBurner) => { 
-      event.shaped(output, [
+    // Metallic Cogwheels
+    let cog = (output, base, metalIngot) => { 
+      event.shapeless(output, [
+        base,
+        metalIngot
+      ])
+    }
+    cog('steampowered:bronze_cogwheel', 'create:shaft', 'alloyed:bronze_ingot')
+    cog('steampowered:cast_iron_cogwheel', 'create:shaft', 'createdeco:cast_iron_ingot')
+    cog('steampowered:steel_cogwheel', 'create:shaft', 'alloyed:steel_ingot')
+    cog('steampowered:bronze_large_cogwheel', 'steampowered:bronze_cogwheel', 'alloyed:bronze_ingot')
+    cog('steampowered:cast_iron_large_cogwheel', 'steampowered:cast_iron_cogwheel', 'createdeco:cast_iron_ingot')
+    cog('steampowered:steel_large_cogwheel', 'steampowered:steel_cogwheel', 'alloyed:steel_ingot')
+    
+    let largeCog = (output, metalIngot) => { 
+      event.shapeless(output, [
+        'create:shaft',
+        metalIngot,
+        metalIngot
+      ])
+    }
+    largeCog('steampowered:bronze_large_cogwheel', 'alloyed:bronze_ingot')
+    largeCog('steampowered:cast_iron_large_cogwheel', 'createdeco:cast_iron_ingot')
+    largeCog('steampowered:steel_large_cogwheel', 'alloyed:steel_ingot')
+
+    // Burners & Boilers
+    let burner = (output, metalSheet) => { 
+      event.recipes.create.mechanical_crafting(output, [
         'III',
         'I I',
         'BBB'
       ], {
         B: 'minecraft:bricks',
-        I: metalBurner
+        I: metalSheet
       })
     }
     burner('steampowered:bronze_burner', 'alloyed:bronze_sheet')
     burner('steampowered:cast_iron_burner', 'createdeco:cast_iron_sheet')
     burner('steampowered:steel_burner', 'alloyed:steel_sheet')
 
-    // Boilers
-    let boiler = (output, metalBoiler) => {
-      event.shaped(output, [
-        ' M ',
+    let boiler = (output, metalSheet) => {
+      event.recipes.create.mechanical_crafting(output, [
+        'MM ',
         'MTM',
-        ' M '
+        ' MM'
       ], {
-        T: 'create:fluid_tank',
-        M: metalBoiler
+        T: 'create:fluid_tank', M: metalSheet
       })
     }
     boiler('steampowered:bronze_boiler', 'alloyed:bronze_sheet')
     boiler('steampowered:cast_iron_boiler', 'createdeco:cast_iron_sheet')
     boiler('steampowered:steel_boiler', 'alloyed:steel_sheet')
 
-    // Engines
+    // Engines & Flywheels
     brassMachine(event,Item.of('steampowered:bronze_steam_engine'), 'alloyed:bronze_block')
-		brassMachine(event,Item.of('steampowered:cast_iron_steam_engine'), 'createdeco:cast_iron_block')
+    brassMachine(event,Item.of('steampowered:cast_iron_steam_engine'), 'createdeco:cast_iron_block')
     brassMachine(event,Item.of('steampowered:steel_steam_engine'), 'alloyed:steel_block')
 
-    // Flywheels
     createMachine('create:flywheel', event, 'steampowered:bronze_flywheel', 'alloyed:bronze_block')
     createMachine('create:flywheel', event, 'steampowered:cast_iron_flywheel', 'createdeco:cast_iron_block')
     createMachine('create:flywheel', event, 'steampowered:steel_flywheel', 'alloyed:steel_block')
   })
 }
+
 
