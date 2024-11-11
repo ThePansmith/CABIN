cbecf0a95bd0fb5eafa7708cad778a99d1f0a118
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-11 18:01:29 -0400

    Steam Powered Blacklist Overhaul

--- overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
+++ overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
@@ -1,14 +1,7 @@
 // Steam Powered
-if (Platform.isLoaded("createaddition")) {
-	global.itemBlacklist.push("steampowered:alternator")
-	global.itemBlacklist.push("steampowered:bronze_cogwheel")
-	global.itemBlacklist.push("steampowered:bronze_large_cogwheel")
-	global.itemBlacklist.push("steampowered:cast_iron_cogwheel")
-	global.itemBlacklist.push("steampowered:cast_iron_large_cogwheel")
-	global.itemBlacklist.push("steampowered:steel_cogwheel")
-	global.itemBlacklist.push("steampowered:steel_large_cogwheel")
-
+if (Platform.isLoaded("steampowered")) {
 	global.randomiumBlacklist.push("steampowered:pressurized_gas_container")
 	global.randomiumBlacklist.push("steampowered:pressurized_steam_container")
 	global.randomiumBlacklist.push("steampowered:bronze_sheet")
+	global.randomiumBlacklist.push("steampowered:alternator") // C:C&A Already has a better alternator, use that one instead
 }
