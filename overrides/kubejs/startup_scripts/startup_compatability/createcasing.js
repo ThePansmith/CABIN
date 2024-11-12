4800d906bd78bc5407f8e59fa3c174694c2b827b
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-12 01:17:00 -0400

    Create Encased Blacklist

---
+++ overrides/kubejs/startup_scripts/startup_compatability/createcasing.js
@@ -0,0 +1,5 @@
+// Create Encased
+if (Platform.isLoaded("createcasing")) {
+	global.itemBlacklist.push("#cabin:encased_shafts")
+	global.randomiumBlacklist.push("createcasing:creative_cogwheel")
+}
