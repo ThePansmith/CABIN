91b26276ef89be0889cc2cf7cb01ebbcf61bf7e9
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-17 01:23:19 -0400

    Create Diesel Generators Compat

---
+++ overrides/kubejs/startup_scripts/startup_compatability/createdieselgenerators.js
@@ -0,0 +1,12 @@
+// Create Diesel Generators
+if (Platform.isLoaded("createdieselgenerators")) {
+	global.itemBlacklist.push("createdieselgenerators:crude_oil_bucket")
+	global.itemBlacklist.push("createdieselgenerators:gasoline_bucket")
+	global.itemBlacklist.push("createdieselgenerators:diesel_bucket")
+	global.itemBlacklist.push("createdieselgenerators:biodiesel_bucket")
+	global.itemBlacklist.push("createdieselgenerators:plant_oil_bucket")
+
+	global.randomiumBlacklist.push("createdieselgenerators:pumpjack_crank")
+	global.randomiumBlacklist.push("createdieselgenerators:huge_diesel_engine")
+	global.randomiumBlacklist.push("createdieselgenerators:oil_scanner")
+}
