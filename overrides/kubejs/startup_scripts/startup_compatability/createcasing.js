d239d46a89c4c56972ac5a80dddf7601ffdfed00
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-16 21:05:11 -0400

    Blacklist Fix

--- overrides/kubejs/startup_scripts/startup_compatability/createcasing.js
+++ overrides/kubejs/startup_scripts/startup_compatability/createcasing.js
@@ -1,5 +1,16 @@
 // Create Encased
 if (Platform.isLoaded("createcasing")) {
-	global.itemBlacklist.push("#cabin:encased_shafts")
+	global.itemBlacklist.push("oak_shaft")
+	global.itemBlacklist.push("spruce_shaft")
+	global.itemBlacklist.push("birch_shaft")
+	global.itemBlacklist.push("jungle_shaft")
+	global.itemBlacklist.push("acacia_shaft")
+	global.itemBlacklist.push("dark_oak_shaft")
+	global.itemBlacklist.push("crimson_shaft")
+	global.itemBlacklist.push("warped_shaft")
+	global.itemBlacklist.push("glass_shaft")
+	global.itemBlacklist.push("brass_shaft")
+	
 	global.randomiumBlacklist.push("createcasing:creative_cogwheel")
+	global.randomiumBlacklist.push("createcasing:mldeg_shaft")
 }
