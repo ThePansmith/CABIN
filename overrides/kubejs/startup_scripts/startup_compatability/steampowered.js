20f07eca5d975e1907762de27e9bdab4c15f932c
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-18 12:52:22 -0400

    Blacklist Rework, again

--- overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
+++ overrides/kubejs/startup_scripts/startup_compatability/steampowered.js
@@ -1,7 +1,9 @@
 // Steam Powered
 if (Platform.isLoaded("steampowered")) {
-	global.randomiumBlacklist.push("steampowered:pressurized_gas_container")
-	global.randomiumBlacklist.push("steampowered:pressurized_steam_container")
-	global.randomiumBlacklist.push("steampowered:bronze_sheet")
-	global.randomiumBlacklist.push("steampowered:alternator") // C:C&A Already has a better alternator, use that one instead
+	global.itemBlacklist.push("steampowered:pressurized_gas_container")
+	global.itemBlacklist.push("steampowered:pressurized_steam_container")
+	global.itemBlacklist.push("steampowered:bronze_sheet")
+	global.itemBlacklist.push("steampowered:alternator") // C:C&A Already has a better alternator, use that one instead
+
+    global.jeiFluidBlacklist.push('steampowered:steam_flowing')
 }
