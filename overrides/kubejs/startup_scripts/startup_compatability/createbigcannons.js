0a17fde3fb12895fc3cd8011a58f8cbcfa670d16
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-19 22:48:09 -0400

    Better Big Cannons Compat (#131)
    * Create Diesel Generators Compat
    
    * Fuel Types Rework
    
    * Improved C:BC Compat
    
    * Extra Tweaks via Datapack
    
    * Mistakes were made
    
    * And again
    
    * And again...
    
    * Files no longer needed
    
    * Blacklist Tweaks

---
+++ overrides/kubejs/startup_scripts/startup_compatability/createbigcannons.js
@@ -0,0 +1,17 @@
+// Big Cannons
+if (Platform.isLoaded("createbigcannons")) {
+	global.itemBlacklist.push("createbigcannons:cast_iron_nugget")
+	global.itemBlacklist.push("createbigcannons:cast_iron_ingot")
+	global.itemBlacklist.push("createbigcannons:cast_iron_block")
+	global.itemBlacklist.push("createbigcannons:molten_steel_bucket")
+	global.itemBlacklist.push("createbigcannons:molten_bronze_bucket")
+
+	global.jeiFluidBlacklist.push('createbigcannons:molten_steel')
+	global.jeiFluidBlacklist.push('createbigcannons:molten_bronze')
+	global.jeiFluidBlacklist.push('createbigcannons:flowing_molten_steel')
+	global.jeiFluidBlacklist.push('createbigcannons:flowing_molten_bronze')
+	global.jeiFluidBlacklist.push('createbigcannons:flowing_molten_cast_iron')
+	global.jeiFluidBlacklist.push('createbigcannons:flowing_molten_nethersteel')
+
+	global.randomiumBlacklist.push("createbigcannons:creative_autocannon_ammo_container")
+}
