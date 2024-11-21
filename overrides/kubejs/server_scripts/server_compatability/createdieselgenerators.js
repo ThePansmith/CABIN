fc5600b81de954b00fbad618d1b4a51d4a7d7b53
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-20 20:57:16 -0400

    Better Fuel Compats & Minor Tweaks

--- overrides/kubejs/server_scripts/server_compatability/createdieselgenerators.js
+++ overrides/kubejs/server_scripts/server_compatability/createdieselgenerators.js
@@ -2,19 +2,8 @@
 if(Platform.isLoaded("createdieselgenerators")) {
   onEvent('fluid.tags', event => {
 
-    // Oil Tag Cleanup
-    event.removeAllTagsFrom('createdieselgenerators:crude_oil')
-    event.removeAllTagsFrom('createdieselgenerators:flowing_crude_oil')
-    event.removeAllTagsFrom('createdieselgenerators:gasoline')
-    event.removeAllTagsFrom('createdieselgenerators:flowing_gasoline')
-    event.removeAllTagsFrom('createdieselgenerators:diesel')
-    event.removeAllTagsFrom('createdieselgenerators:flowing_diesel')
-    event.removeAllTagsFrom('createdieselgenerators:biodiesel')
-    event.removeAllTagsFrom('createdieselgenerators:flowing_biodiesel')
-    event.removeAllTagsFrom('createdieselgenerators:plant_oil')
-    event.removeAllTagsFrom('createdieselgenerators:flowing_plant_oil')
-
     // Pumpjack Output
+    event.remove('createdieselgenerators:pumpjack_output', 'createdieselgenerators:crude_oil')
     event.add('createdieselgenerators:pumpjack_output', 'thermal:crude_oil')
   })
 
@@ -43,6 +32,14 @@ if(Platform.isLoaded("createdieselgenerators")) {
     copperMachine(event, Item.of('createdieselgenerators:pumpjack_hole', 1))
     invarMachine(event, Item.of('createdieselgenerators:pumpjack_crank', 1), 'create:zinc_block')
     invarMachine(event, Item.of('createdieselgenerators:oil_scanner', 1), 'ae2:charged_certus_quartz_crystal')
+    event.replaceInput({
+      id: 'createdieselgenerators:crafting/pumpjack_bearing'},
+      'create:andesite_alloy',
+      'thermal:invar_ingot')
+    event.replaceInput({
+      id: 'createdieselgenerators:crafting/pumpjack_head'},
+      'create:zinc_ingot',
+      'thermal:invar_ingot')
 
     // Oil Engines
     event.remove({id: 'createdieselgenerators:crafting/engine_piston'}) // This one uses a Shaft instead of an Iron Rod
@@ -50,8 +47,8 @@ if(Platform.isLoaded("createdieselgenerators")) {
     event.recipes.create.mechanical_crafting('createdieselgenerators:diesel_engine', [
       ' BLB ',
       'PPSPP',
-      ' BTB '],
-      { L: 'createdieselgenerators:lighter',
+      ' BTB '],{
+        L: 'createdieselgenerators:lighter',
         P: 'createdieselgenerators:engine_piston',
         B: 'create:brass_ingot',
         S: 'create:shaft',
@@ -65,45 +62,97 @@ if(Platform.isLoaded("createdieselgenerators")) {
     event.shaped('createdieselgenerators:chip_wood_block', [
       'CCC',
       'CCC',
-      'CCC'],
-      {C: 'createdieselgenerators:wood_chip'} // Now uses 9 instead of 4 in order to prevent duping exploits
-    )
+      'CCC'],{
+      C: 'createdieselgenerators:wood_chip' // Now uses 9 instead of 4 in order to prevent duping exploits
+    })
 
-    // Miscelaneus Tweaks
+    // Asphalt from Bucket
     event.replaceInput({
-      id: 'createdieselgenerators:crafting/pumpjack_bearing'},
-      'create:andesite_alloy',
-      'thermal:invar_ingot')
-    event.replaceInput({
-      id: 'createdieselgenerators:crafting/pumpjack_head'},
-      'create:zinc_ingot',
-      'thermal:invar_ingot')
-    event.replaceInput({
-        id: 'createdieselgenerators:crafting/asphalt_block'},
+      id: 'createdieselgenerators:crafting/asphalt_block'},
         'createdieselgenerators:crude_oil_bucket',
         'thermal:crude_oil_bucket')
+  })
 
-    // Frog Leg Fermentation (Because why not?)
-    event.custom({
-      "type": "createdieselgenerators:basin_fermenting",
-      "ingredients": [
-        {"item": "quark:frog_leg"},
-        {"tag": "forge:nuggets/gold"},
-        {"tag": "forge:nuggets/gold"},
-        {"tag": "forge:nuggets/gold"},
-        {"tag": "forge:nuggets/gold"},
-        {"tag": "forge:nuggets/gold"},
-        {"fluid": "create:potion",
-          "nbt": {"Potion": "minecraft:mundane"},
-          "amount": 100}
-      ],
-      "processingTime": 400,
-      "results": [
-        {"item": "quark:golden_frog_leg"},
-        {"fluid": "create:potion",
-          "nbt": {"Potion": "minecraft:thick"},
-          "amount": 100}
-      ]
+  onEvent('server.datapack.first', event => {
+
+    // Thermal's Fuels
+    // There's gotta be a way to compact all these in a single function, but I couldn't figure it out
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/creosote`, {
+      "fluid": "thermal:creosote",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 2048.0, "burn_rate": 2},
+      "modular": { "speed": 32.0, "strength": 3072.0, "burn_rate": 6},
+      "huge": {"speed": 32.0, "strength": 4096.0, "burn_rate": 8}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/heavy_oil`, {
+      "fluid": "thermal:heavy_oil",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 2048.0, "burn_rate": 2},
+      "modular": { "speed": 96.0, "strength": 6144.0, "burn_rate": 3},
+      "huge": {"speed": 128.0, "strength": 12288.0, "burn_rate": 4}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/light_oil`, {
+      "fluid": "thermal:light_oil",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 3072.0, "burn_rate": 2},
+      "modular": { "speed": 128.0, "strength": 6144.0, "burn_rate": 3},
+      "huge": {"speed": 96.0, "strength": 6144.0, "burn_rate": 6}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/refined_fuel`, {
+      "fluid": "#thermal:refined_fuel",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 4096.0, "burn_rate": 1},
+      "modular": { "speed": 128.0, "strength": 8192.0, "burn_rate": 2},
+      "huge": {"speed": 128.0, "strength": 16384.0, "burn_rate": 4}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/tree_oil`, {
+      "fluid": "thermal:tree_oil",
+      "sound_speed": 1,
+      "normal": {"speed": 32.0, "strength": 1024.0, "burn_rate": 1},
+      "modular": { "speed": 64.0, "strength": 2048.0, "burn_rate": 2},
+      "huge": {"speed": 64.0, "strength": 3072.0, "burn_rate": 6}
+    })
+
+    // Forge's Fuel Tags
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/biodiesel`, {
+      "fluid": "#forge:biofuel",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 4096.0, "burn_rate": 3},
+      "modular": { "speed": 128.0, "strength": 8192.0, "burn_rate": 6},
+      "huge": {"speed": 128.0, "strength": 12288.0, "burn_rate": 12}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/diesel`, {
+      "fluid": "#forge:diesel",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 2048.0, "burn_rate": 2},
+      "modular": { "speed": 96.0, "strength": 6144.0, "burn_rate": 3},
+      "huge": {"speed": 128.0, "strength": 12288.0, "burn_rate": 4}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/ethanol`, {
+      "fluid": "#forge:ethanol",
+      "sound_speed": 2,
+      "normal": {"speed": 64.0, "strength": 3072.0, "burn_rate": 3},
+      "modular": { "speed": 96.0, "strength": 6144.0, "burn_rate": 6},
+      "huge": {"speed": 96.0, "strength": 6144.0, "burn_rate": 12}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/gasoline`, {
+      "fluid": "#forge:gasoline",
+      "sound_speed": 3,
+      "normal": {"speed": 64.0, "strength": 3072.0, "burn_rate": 2},
+      "modular": { "speed": 128.0, "strength": 6144.0, "burn_rate": 3},
+      "huge": {"speed": 96.0, "strength": 6144.0, "burn_rate": 6}
+    })
+    event.addJson(`createdieselgenerators:diesel_engine_fuel_types/plantoil`, {
+      "fluid": "#forge:plantoil",
+      "sound_speed": 1,
+      "normal": {"speed": 32.0, "strength": 1024.0, "burn_rate": 1},
+      "modular": { "speed": 64.0, "strength": 2048.0, "burn_rate": 2},
+      "huge": {"speed": 64.0, "strength": 3072.0, "burn_rate": 6}
+    })
+
+    // Disable Beyond Earth's oil wells 
+    event.addJson(`beyond_earth:tags/worldgen/biome/has_structure/oil_well_biomes`, {
+      "replace": true, "values": []
     })
   })
 }
