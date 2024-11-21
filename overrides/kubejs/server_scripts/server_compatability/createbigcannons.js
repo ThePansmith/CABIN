bd290fc52cac1bc42caa57dc2673f876753bf0be
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-11-20 18:08:31 -0600

    better big cannons compat changes and smeltery helper functions

--- overrides/kubejs/server_scripts/server_compatability/createbigcannons.js
+++ overrides/kubejs/server_scripts/server_compatability/createbigcannons.js
@@ -5,35 +5,26 @@ if(Platform.isLoaded("createbigcannons")) {
 		event.add('forge:storage_blocks/cast_iron', 'createbigcannons:cast_iron_block')
 		event.add('forge:storage_blocks/nethersteel', 'createbigcannons:nethersteel_block')
 	})
+
+	onEvent('fluid.tags', event => {
+		//Fluid tag to measure by ingots instead of buckets in the smeltery
+		event.add('tconstruct:tooltips/metal', 'createbigcannons:molten_bronze')
+		event.add('tconstruct:tooltips/metal', 'createbigcannons:molten_cast_iron')
+		event.add('tconstruct:tooltips/metal', 'createbigcannons:molten_steel')
+		event.add('tconstruct:tooltips/metal', 'createbigcannons:molten_nethersteel')
+	})
 	
 	onEvent('recipes', event => {
 
-		// Smeltery
+		//Basin Foundry could be nice to have but we already have 2 ways of melting ingots into fluids
 		event.remove({id: 'createbigcannons:basin_foundry_lid'})
 		event.remove({mod: 'createbigcannons', type: 'createbigcannons:melting'})
 
-		let smeltery = (input, output, amount, time, temp) => {
-			event.custom({
-				"type": "tconstruct:melting",
-				"ingredient": {"tag": input},
-				"result": {
-					"fluid": output,
-					"amount": amount
-				},
-				"temperature": temp,
-				"time": time
-		})}
-		smeltery("forge:nuggets/cast_iron", "createbigcannons:molten_cast_iron", 10, 16, 650)
-		smeltery("forge:ingots/cast_iron", "createbigcannons:molten_cast_iron", 90, 48, 650)
-		smeltery("forge:storage_blocks/cast_iron", "createbigcannons:molten_cast_iron", 810, 144, 650)
-		smeltery("forge:plates/cast_iron", "createbigcannons:molten_cast_iron", 90, 48, 650)
-
-		smeltery("forge:nuggets/nethersteel", "createbigcannons:molten_nethersteel", 10, 27, 1375)
-		smeltery("forge:ingots/nethersteel", "createbigcannons:molten_nethersteel", 90, 80, 1375)
-		smeltery("forge:storage_blocks/nethersteel", "createbigcannons:molten_nethersteel", 810, 240, 1375)
-
 		// Alloying
-		event.remove({mod: 'createbigcannons', type: 'create:mixing', output: 'createbigcannons:nethersteel_ingot'})
+		event.remove([
+			{id: 'createbigcannons:mixing/alloy_nethersteel_cast_iron'},
+			{id: 'createbigcannons:mixing/alloy_nethersteel_steel'}
+		])
 
 		let debrisAlloy = (moltenMetal, amount) => {
 			event.custom({
@@ -54,39 +45,14 @@ if(Platform.isLoaded("createbigcannons")) {
 		debrisAlloy("forge:molten_cast_iron", 720)
 		debrisAlloy("tconstruct:molten_steel", 360)
 		
-		// Casting & Cooling
-		event.remove({mod: 'createbigcannons', type: 'create:compacting'})
-		
-		let blockCasting = (moltenMetal, block, time) => {
-			event.custom({
-				"type": "tconstruct:casting_basin",
-				"fluid": {
-					"tag": moltenMetal,
-					"amount": 810
-				},
-				"result": block,
-				"cooling_time": time
-		})}
-		blockCasting("forge:molten_cast_iron", "createdeco:cast_iron_block", 144)
-		blockCasting("forge:molten_nethersteel", "createbigcannons:nethersteel_block", 240)
+		// Casting & Melting
+		metalCasting(event, 'cast_iron', 144)
+		metalCasting(event, 'nethersteel', 240)
 
-		let ingotCooling = (moltenMetal, ingot) => {
-			event.custom({
-				"type": "thermal:chiller",
-				"ingredients": [{
-					"fluid_tag": moltenMetal,
-					"amount": 90
-				},{
-					"item": "thermal:chiller_ingot_cast"
-				}],
-				"result": [{
-					"item": ingot,
-					"count": 1
-				}],
-				"energy": 5000
-		})}
-		ingotCooling("forge:molten_cast_iron", "createdeco:cast_iron_ingot")
-		ingotCooling("forge:molten_nethersteel", "createbigcannons:nethersteel_ingot")
+		metalMelting(event, 'cast_iron', 'createbigcannons:molten_cast_iron', 144, 650)
+		metalMelting(event, 'nethersteel', 'createbigcannons:molten_nethersteel', 240, 1375)
+
+		event.remove({mod: 'createbigcannons', type: 'create:compacting'})
 		
 		// Gunpowder Packing
 		// These two recipes were deleted by a previous event, so we have to recreate them
@@ -95,15 +61,23 @@ if(Platform.isLoaded("createbigcannons")) {
 		event.recipes.create.compacting('createbigcannons:guncotton',
 			['#createbigcannons:guncotton', '#createbigcannons:guncotton', '#createbigcannons:guncotton'])
 
-		// Cast Iron Metal
-		event.replaceOutput({}, 'createbigcannons:cast_iron_ingot','createdeco:cast_iron_ingot')
-		event.replaceOutput({}, 'createbigcannons:cast_iron_nugget','createdeco:cast_iron_nugget')
-		event.replaceOutput({}, 'createbigcannons:cast_iron_block','createdeco:cast_iron_block')
+		// Duplicate Cast Iron recipes
+		event.remove([
+			{id: 'minecraft:cast_iron_nugget'},
+			{id: 'minecraft:cast_iron_ingot_from_nuggets'},
+			{id: 'minecraft:cast_iron_ingot_from_block'},
+			{id: 'minecraft:cast_iron_block'}
+		])
 
 		// Cannon Machines
 		andesiteMachine(event, Item.of('createbigcannons:yaw_controller', 1))
 		andesiteMachine(event, Item.of('createbigcannons:cannon_builder', 1), 'create:mechanical_bearing')
 		andesiteMachine(event, Item.of('createbigcannons:cannon_loader', 1), 'create:mechanical_piston')
 		andesiteMachine(event, Item.of('createbigcannons:cannon_drill', 1), 'create:fluid_tank')
+	})
+
+	//CreateDeco Cast Iron Block as a variant
+	onEvent('server.datapack.first', event => {
+		addChiselingRecipe(event, 'kubejs:chiseling_recipes/compat/createbigcannons/cast_iron', ["createdeco:cast_iron_block", "createbigcannons:cast_iron_block"])
 	})
 } 
--- overrides/kubejs/startup_scripts/startup_compatability/createbigcannons.js
+++ overrides/kubejs/startup_scripts/startup_compatability/createbigcannons.js
@@ -1,8 +1,8 @@
 // Big Cannons
 if (Platform.isLoaded("createbigcannons")) {
-	global.itemBlacklist.push("createbigcannons:cast_iron_nugget")
+	global.itemBlacklist.push('createbigcannons:basin_foundry_lid')
+	//global.itemBlacklist.push("createbigcannons:cast_iron_nugget")
 	global.itemBlacklist.push("createbigcannons:cast_iron_ingot")
-	global.itemBlacklist.push("createbigcannons:cast_iron_block")
 	global.itemBlacklist.push("createbigcannons:molten_steel_bucket")
 	global.itemBlacklist.push("createbigcannons:molten_bronze_bucket")
 
@@ -13,5 +13,6 @@ if (Platform.isLoaded("createbigcannons")) {
 	global.jeiFluidBlacklist.push('createbigcannons:flowing_molten_cast_iron')
 	global.jeiFluidBlacklist.push('createbigcannons:flowing_molten_nethersteel')
 
+	global.randomiumBlacklist.push("createbigcannons:traffic_cone")
 	global.randomiumBlacklist.push("createbigcannons:creative_autocannon_ammo_container")
 }
