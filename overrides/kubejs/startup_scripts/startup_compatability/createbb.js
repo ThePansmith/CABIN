cea2f997e85085ad22e59fbe49551b24e06f6ccf
CapNapu <110067479+CapNapu@users.noreply.github.com>
2024-11-23 04:22:07 -0400

    C:BB Flowing Liquids Blacklist

--- overrides/kubejs/startup_scripts/startup_compatability/createbb.js
+++ overrides/kubejs/startup_scripts/startup_compatability/createbb.js
@@ -2,4 +2,15 @@
 if (Platform.isLoaded("createbb")) {
 	global.itemBlacklist.push("createbb:crushed_copper")
 	global.itemBlacklist.push("createbb:crushed_zinc")
+
+	global.jeiFluidBlacklist.push('createbb:flowing_liquid_blue_methamphetamine')
+	global.jeiFluidBlacklist.push('createbb:flowing_liquid_methamphetamine')
+	global.jeiFluidBlacklist.push('createbb:flowing_methylamine')
+	global.jeiFluidBlacklist.push('createbb:flowing_methanol')
+	global.jeiFluidBlacklist.push('createbb:flowing_hydrogen')
+	global.jeiFluidBlacklist.push('createbb:flowing_oxygen')
+	global.jeiFluidBlacklist.push('createbb:flowing_ammonia')
+	global.jeiFluidBlacklist.push('createbb:flowing_phenylacetic_acid')
+	global.jeiFluidBlacklist.push('createbb:flowing_acetic_anhydride')
+	global.jeiFluidBlacklist.push('createbb:flowing_phenylacetone')
 }
