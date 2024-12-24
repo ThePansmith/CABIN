db4cdc4097f4ec15e85b2c1ae1f94879208f1412
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-12-24 14:56:42 -0600

    Design and Decor compatability tweaks

--- overrides/kubejs/startup_scripts/startup_compatability/design_decor.js
+++ overrides/kubejs/startup_scripts/startup_compatability/design_decor.js
@@ -1,8 +1,8 @@
-// Thermal Systeams
 if (Platform.isLoaded("design_decor")) {
-	//global.itemBlacklist.push("design_decor:aluminium_boiler")
-	//global.itemBlacklist.push("design_decor:aluminium_boiler_large")
-	//global.itemBlacklist.push("design_decor:aluminium_large_chain")
+	global.itemBlacklist.push("design_decor:aluminium_boiler")
+	global.itemBlacklist.push("design_decor:aluminium_boiler_large")
+	global.itemBlacklist.push("design_decor:aluminium_large_chain")
+	
 	global.itemBlacklist.push("design_decor:strong_bronze_large_chain")
 	global.itemBlacklist.push("design_decor:mithril_large_chain")
 	global.itemBlacklist.push("design_decor:tin_large_chain")
