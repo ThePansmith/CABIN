e196943efc42e101f4808e9ae8d2c3be4903846a
roidrole <83431927+roidrole@users.noreply.github.com>
2025-01-26 21:32:03 -0500

    Fix potions rarity

--- overrides/kubejs/startup_scripts/mods/reliquary.js
+++ overrides/kubejs/startup_scripts/mods/reliquary.js
@@ -1,8 +1,8 @@
 if(Platform.isLoaded("reliquary")){
     onEvent('fluid.registry', event => {
-        event.create('glowing_water').thinTexture(0x436ee6)//.bucketColor(0x00FFFF)
-        event.create('angelheart_vial').thinTexture(0xcc6d3d)
-        event.create('aphrodite_potion').thinTexture(0xb5b138)
-        event.create('fertile_potion').thinTexture(0x85d842)
+        event.create('glowing_water').thinTexture(0x436ee6).rarity("common")
+        event.create('angelheart_vial').thinTexture(0xcc6d3d).rarity("common")
+        event.create('aphrodite_potion').thinTexture(0xb5b138).rarity("common").displayName("Aphrodite's Serum")
+        event.create('fertile_potion').thinTexture(0x85d842).rarity("common")
     })
 }
