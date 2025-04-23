42d9c275cae9bc856f4f838c046b366e4a24cac0
OuOu2021 <1113117424@qq.com>
2025-04-23 20:12:34 +0800

    quote

--- kubejs/server_scripts/auto_reload_lang.js
+++ kubejs/server_scripts/auto_reload_lang.js
@@ -2,7 +2,7 @@ global._langReloaded = global._langReloaded || false;
 
 ServerEvents.loaded(event => {
     if (!global._langReloaded) {
-        event.server.runCommand('/kubejs reload lang')
+        event.server.runCommand("/kubejs reload lang")
         global._langReloaded = true
     }
 });
