0d00b90c44fec68302375fb7c4e73385a951df70
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-05-18 22:14:59 -0500

    next patch

--- kubejs/startup_scripts/startup_compatability/createcasing.js
+++
@@ -1,6 +0,0 @@
-// Create Encased
-if (Platform.isLoaded("createcasing")) {
-    StartupEvents.registry("item", e => {
-        // e.create('createcasing:processing_chorium', 'create:sequenced_assembly')
-    })
-}
