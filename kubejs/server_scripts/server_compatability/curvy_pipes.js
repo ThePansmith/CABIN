dc7e5f390dce4f7bda9bbe9f57060e3f383e03c4
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-06-22 04:45:38 -0500

    fix conversions

--- kubejs/server_scripts/server_compatability/curvy_pipes.js
+++ kubejs/server_scripts/server_compatability/curvy_pipes.js
@@ -1,8 +1,30 @@
 if (Platform.isLoaded("curvy_pipes")) {
     // Downcrafting/conversion recipes are done in curvy pipes config
     ServerEvents.recipes(event => {
-        brassMachine(event, Item.of("curvy_pipes:small_item_pipe", 8), "create:package_frogport")
-        copperMachine(event, Item.of("curvy_pipes:small_fluid_pipe", 4), "minecraft:lapis_block")
+        // copied machine function but it doesn't remove existing recipes to keep conversions
+        const safeMachine = (machineItem, event, outputIngredient, inputIngredient) => {
+            machineItem = Ingredient.of(machineItem)
+            outputIngredient = Item.of(outputIngredient)
+            inputIngredient = Ingredient.of(inputIngredient)
+            event.custom({
+                "type": "create:item_application",
+                "ingredients": [
+                    machineItem.toJson(),
+                    inputIngredient.toJson()
+                ],
+                "results": (outputIngredient.isBlock() && outputIngredient.getCount() > 1) ?
+                    [
+                        outputIngredient.withCount(1).toJson(),
+                        outputIngredient.withCount(outputIngredient.getCount() - 1).toJson()
+                    ] : [
+                        outputIngredient.toJson()
+                    ]
+            })
+        }
+
+        safeMachine("kubejs:brass_machine", event, Item.of("curvy_pipes:small_item_pipe", 8), "create:package_frogport")
+        safeMachine("kubejs:copper_machine", event, Item.of("curvy_pipes:small_fluid_pipe", 4), "minecraft:lapis_block")
+
         event.shaped("8x curvy_pipes:small_energy_pipe", ["P", "M", "P"], {
             P: "thermal:invar_ingot",
             M: "minecraft:redstone"
