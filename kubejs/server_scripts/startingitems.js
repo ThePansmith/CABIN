bbee0d8dd298d18099ec6159ccb136229585043d
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-10-21 23:45:09 -0500

    Move tinkers guide to QB
    looks slightly nicer that way

--- kubejs/server_scripts/startingitems.js
+++ kubejs/server_scripts/startingitems.js
@@ -6,7 +6,8 @@ PlayerEvents.loggedIn(event => {
     // Check if player doesn't have "starting_items" stage yet
     if (!event.player.stages.has("starting_items")) {
     // Add the stage
-        event.player.stages.add("starting_items")
+    event.player.stages.add("starting_items")
+    event.player.inventory.clear()
         // Give qb to player
         event.player.give("ftbquests:book")
     }
