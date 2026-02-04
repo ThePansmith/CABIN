464fe965ea9eaac6475852fc802d9d19e7d27fb7
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2026-02-03 18:47:51 -0600

    reliquary hiding uses jei

--- kubejs/client_scripts/mods/reliquary.js
+++ kubejs/client_scripts/mods/reliquary.js
@@ -1,27 +1,11 @@
 if (Platform.isLoaded("reliquary")) {
-    ClientEvents.highPriorityAssets(event => {
-        let json = {
-            "filters": [
-                // Hide alkahestry in the crafting category because alkahestry has its own emi categories
-                {
-                    "id": "/reliquary:alkahestry\\//",
-                    "category": "minecraft:crafting"
-                },
-                {
-                    "id": "/kubejs:alkahestry\\//",
-                    "category": "minecraft:crafting"
-                },
-                // Hide removed alkahestry recipes because they stick aroung for some reason?
-                {
-                    "id": "jei:/reliquary/alkahestry/crafting/tin_ingot",
-                    "category": "reliquary:alkahestry_crafting"
-                },
-                {
-                    "id": "jei:/reliquary/alkahestry/crafting/silver_ingot",
-                    "category": "reliquary:alkahestry_crafting"
-                }
-            ]
-        }
-        event.add("emi:recipe/filters/reliquary", json)
+    JEIEvents.removeRecipes(event => {
+        // Hide alkahestry recipes from the vanilla crafting category
+        event.remove("minecraft:crafting", /reliquary:alkahestry\//)
+        event.remove("minecraft:crafting", /kubejs:alkahestry\//)
+
+        // Stuck around for some reason
+        event.remove("reliquary:alkahestry_crafting", "reliquary:alkahestry/crafting/tin_ingot")
+        event.remove("reliquary:alkahestry_crafting", "reliquary:alkahestry/crafting/silver_ingot")
     })
 }
